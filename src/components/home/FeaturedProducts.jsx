7554c0d8ca13d359d6b875ba99a95da81be9ddd3
SinMobile <sinmobile10@gmail.com>
2024-02-17 10:55:22 +0330

    Products Section Design

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -1,9 +1,113 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 class FeaturedProducts extends Component {
     render() {
         return (
-            <div>FeaturedProducts</div>
+            <Fragment>
+                <Container className="text-center" fluid={true}>
+                    <div className="section-title text-center mb-55">
+                        <h2>
+                            FEATURED PRODUCTS
+                        </h2>
+                        <p>
+                            Collection That You May Like
+                        </p>
+                    </div>
+                    <Row>
+                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center"
+                                     src="https://rukminim2.flixcart.com/image/416/416/ktketu80/mobile/6/n/d/iphone-13-mlpg3hn-a-apple-original-imag6vpyghayhhrh.jpeg?q=70&crop=false"
+                                     alt=""/>
+                                <Card.Body>
+                                    <p className="product-name-on-card">
+                                        Apple iPhone 13 (Starlight, 128 GB)
+                                    </p>
+                                    <p className="product-price-on-card">
+                                        Price : $999
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={2} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center"
+                                     src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70&crop=false"
+                                     alt=""/>
+                                <Card.Body>
+                                    <p className="product-name-on-card">
+                                        Apple iPhone 15 (Black, 128 GB)
+                                    </p>
+                                    <p className="product-price-on-card">
+                                        Price : $999
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={3} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center"
+                                     src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/3/5/l/-original-imaghx9qmgqsk9s4.jpeg?q=70&crop=false"
+                                     alt=""/>
+                                <Card.Body>
+                                    <p className="product-name-on-card">
+                                        Apple iPhone 14 (Blue, 128 GB)
+                                    </p>
+                                    <p className="product-price-on-card">
+                                        Price : $999
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={4} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center"
+                                     src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/a/c/k/-original-imagtc5fuzkvczr7.jpeg?q=70&crop=false"
+                                     alt=""/>
+                                <Card.Body>
+                                    <p className="product-name-on-card">
+                                        Apple iPhone 15 (Pink, 128 GB)
+                                    </p>
+                                    <p className="product-price-on-card">
+                                        Price : $999
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={5} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center"
+                                     src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/u/f/-original-imaghxa5hvapbfds.jpeg?q=70&crop=false"
+                                     alt=""/>
+                                <Card.Body>
+                                    <p className="product-name-on-card">
+                                        Apple iPhone 14 (Purple, 128 GB)
+                                    </p>
+                                    <p className="product-price-on-card">
+                                        Price : $999
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={6} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center"
+                                     src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/9/e/e/-original-imaghx9q5rvcdghy.jpeg?q=70&crop=false"
+                                     alt=""/>
+                                <Card.Body>
+                                    <p className="product-name-on-card">
+                                        Apple iPhone 14 (Midnight, 128 GB)
+                                    </p>
+                                    <p className="product-price-on-card">
+                                        Price : $999
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
         )
     }
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React, {Component, Fragment} from 'react'
+import FeaturedProducts from "../components/home/FeaturedProducts";
 
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
-                <h1>This is Home Page</h1>
+                <FeaturedProducts />
             </Fragment>
         )
     }
