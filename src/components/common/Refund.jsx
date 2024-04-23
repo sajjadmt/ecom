780a949e7eae2e56c77d11c9cc3740e89d31bdc7
sajjadmt <momenitabar@gmail.com>
2024-04-23 10:38:54 +0330

    Set CSS PlacerHolder Loading For Refund.jsx

--- src/components/common/Refund.jsx
+++ src/components/common/Refund.jsx
@@ -3,6 +3,7 @@ import {Col, Container, Row} from "react-bootstrap";
 import ReactHtmlParser from 'react-html-parser';
 import axios from "axios";
 import AppURL from "../../api/AppURL";
+import RefundLoading from "./PlaceHolder/RefundLoading";
 
 class Refund extends Component {
 
@@ -10,6 +11,8 @@ class Refund extends Component {
         super();
         this.state = {
             refund: '',
+            isLoading: '',
+            mainDiv: 'd-none',
         }
     }
 
@@ -21,13 +24,17 @@ class Refund extends Component {
                     let refund = (response.data)[0]['refund'];
                     this.setState({
                         refund: refund,
+                        isLoading: 'd-none',
+                        mainDiv: '',
                     });
                     sessionStorage.setItem("RefundInfo", refund);
                 }
             }).catch()
         } else {
             this.setState({
-                refund: RefundInfoSession
+                refund: RefundInfoSession,
+                isLoading: 'd-none',
+                mainDiv: '',
             });
         }
     }
@@ -35,13 +42,18 @@ class Refund extends Component {
     render() {
         return (
             <Fragment>
-                <Container>
-                    <Row className="p-2">
-                        <Col className="shadow-sm mt-2 bg-white" lg={12} md={12} sm={12} xs={12}>
+                <RefundLoading isloading={this.state.isLoading}/>
+                <div className={this.state.mainDiv}>
+                    <Container>
+                        <br/>
+                        <br/>
+                        <Row className="p-2">
+                            <Col className="shadow-sm mt-2 bg-white" lg={12} md={12} sm={12} xs={12}>
                                 {ReactHtmlParser(this.state.refund)}
-                        </Col>
-                    </Row>
-                </Container>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </Fragment>
         )
     }
