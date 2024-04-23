3dba9902040e6f97f5efd7f1bbff02d8c2078203
sajjadmt <momenitabar@gmail.com>
2024-04-23 09:35:23 +0330

    Set CSS PlacerHolder Loading For Privacy.jsx

--- src/components/common/Privacy.jsx
+++ src/components/common/Privacy.jsx
@@ -3,13 +3,16 @@ import {Col, Container, Row} from "react-bootstrap";
 import axios from "axios";
 import AppURL from "../../api/AppURL";
 import ReactHtmlParser from 'react-html-parser';
+import PrivacyLoading from "./PlaceHolder/PrivacyLoading";
 
 class Privacy extends Component {
 
     constructor() {
         super();
         this.state = {
-            privacy: ''
+            privacy: '',
+            isLoading: '',
+            mainDiv: 'd-none',
         }
     }
 
@@ -20,9 +23,11 @@ class Privacy extends Component {
                 if (response.status === 200) {
                     let privacy = (response.data)[0]['privacy'];
                     this.setState({
-                        privacy: privacy
+                        privacy: privacy,
+                        isLoading: 'd-none',
+                        mainDiv: '',
                     });
-                    sessionStorage.setItem("PrivacyInfo",privacy);
+                    sessionStorage.setItem("PrivacyInfo", privacy);
                 }
             }).catch()
         } else {
@@ -35,13 +40,18 @@ class Privacy extends Component {
     render() {
         return (
             <Fragment>
-                <Container>
-                    <Row className="p-2">
-                        <Col className="shadow-sm mt-2 bg-white" lg={12} md={12} sm={12} xs={12}>
-                            {ReactHtmlParser(this.state.privacy)}
-                        </Col>
-                    </Row>
-                </Container>
+                <PrivacyLoading isLoading={this.state.isLoading}/>
+                <div className={this.state.mainDiv}>
+                    <Container>
+                        <Row className="p-2">
+                            <Col className="shadow-sm mt-2 bg-white" lg={12} md={12} sm={12} xs={12}>
+                                <br/>
+                                <br/>
+                                {ReactHtmlParser(this.state.privacy)}
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </Fragment>
         )
     }
